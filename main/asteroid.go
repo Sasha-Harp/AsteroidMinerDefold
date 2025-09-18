a6a0d753ac6549ad7d715e8b0c2b90253daf4520
Sasha Harp <sashaharp@bazzite.fritz.box>
2025-09-18 08:06:04 +0200

    initial commit

---
+++ main/asteroid.go
@@ -0,0 +1,44 @@
+components {
+  id: "asteroid"
+  component: "/main/asteroid.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"asteroid0\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/game.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.07
+    y: 0.07
+    z: 0.07
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"asteroids\"\n"
+  "mask: \"player\"\n"
+  "mask: \"enemies\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 41.6308\n"
+  "}\n"
+  ""
+}
---
+++ main/lazer.go
@@ -0,0 +1,4 @@
+components {
+  id: "lazer"
+  component: "/assets/lazer.particlefx"
+}
