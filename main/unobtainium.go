3fe46ac62451bacb9bc09c28f73b1dafdc197ec4
Sasha Harp <sashaharp@gmail.com>
2025-10-08 01:45:13 +0200

    added unobtainium

---
+++ main/unobtainium.go
@@ -0,0 +1,39 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"unobtainium\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/game.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.02
+    y: 0.02
+    z: 0.02
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"collectable\"\n"
+  "mask: \"player\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 21.100525\n"
+  "}\n"
+  ""
+}
